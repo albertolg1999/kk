ed00cf684f1f68d99efd2da93ad5e69daf4c84c1
Alberto <albertolaingijon@gmail.com>
2021-06-04 04:27:10 +0200

    mejora

---
+++ voteExtractorRest/src/main/java/com/example/voteExtractorRest/VoteExtractorRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.voteExtractorRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteExtractorRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteExtractorRestApplication.class, args);
+	}
+
+}
---
+++ voteExtractorRest/src/test/java/com/example/voteExtractorRest/VoteExtractorRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voteExtractorRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteExtractorRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
